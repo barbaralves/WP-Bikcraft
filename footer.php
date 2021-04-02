a0867723d46168034cafde86ee6af88d6ef887d1
Bárbara Alves <barbaraalves343@gmail.com>
2021-04-02 15:22:55 -0300

    Separando cabeçalho e rodapé em arquivos

---
+++ footer.php
@@ -0,0 +1,49 @@
+    <div class="quebra">
+        <blockquote class="quote-externo container">
+            <p>"O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana."</p>
+            <cite>WILLIAM MORRIS</cite>
+        </blockquote>
+    </div>
+
+    <footer>
+        <div class="footer">
+            <div class="container">
+                <div class="grid-8 footer-historia">
+                    <h3>Nossa História</h3>
+                    <p>Quando iniciamos a Bikcraft queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>    
+                </div>
+                <div class="grid-4 footer-contato">
+                    <h3>Contato</h3>
+                    <ul>
+                        <li>- 21 23232-4343</li>
+                        <li>- contato@bikcraft.com</li>
+                        <li>- Botafogo - RJ</li>
+                    </ul>
+                </div>
+                <div class="grid-4 footer-rede">
+                    <h3>Redes Sociais</h3>
+                    <ul>
+                        <li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.svg" alt="Facebook Bikcraft"></a></li>
+                        <li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.svg" alt="Instagram Bikcraft"></a></li>
+                        <li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.svg" alt="Twitter Bikcraft"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Fim informações da loja -->
+
+        <div class="copy">
+            <div class="container">
+                <p class="grid-16">Bikcraft 2021 - Alguns direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./js/simple-anime.js"></script>
+    <script src="./js/script.js"></script>
+    
+    <!-- Início Wordpress Footer -->
+    <?php wp_footer(); ?>
+
+</body>
+</html>
