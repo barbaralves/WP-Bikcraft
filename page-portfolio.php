654788062c137b5579b8991f9bcd7cbcee71b230
Bárbara Alves <barbaraalves343@gmail.com>
2021-04-06 14:10:03 -0300

    Alterando conteúdo da página portfólio por funções WP

--- page-portfolio.php
+++ page-portfolio.php
@@ -9,18 +9,12 @@ get_header();
 
     <!-- <section class="container fadeInDown" data-slide="quote" data-anime="1200"> -->
     <section class="container">
-        <blockquote class="quote-cliente">
-            <p>"Pedalar sempre foi a minha paixão, o que o pessoal da Bikcraft fez foi intensificar o meu amor por esta atividade. Recomendo à todos que amo."</p>
-            <cite>Barbara Moss</cite>
-        </blockquote>
-        <blockquote class="quote-cliente">
-            <p>"Nada melhor do que dar um rolê com a minha Bikcraft na orla. Essa é a minha companheira mais fiel, nunca me traiu e está sempre a minha disposição."</p>
-            <cite>Jhony Rato</cite>
-        </blockquote>
-        <blockquote class="quote-cliente">
-            <p>"Aqueles que ainda não possuem uma Bikcraft, não sabem o que estão perdendo. A precisão é absurda e a velocidade transcendental. Nunca vida nada igual."</p>
-            <cite>Bernardo</cite>
-        </blockquote>
+        <?php if(have_rows('quote_portfolio')) : while(have_rows('quote_portfolio')) : the_row(); ?>
+                <blockquote class="quote-cliente">
+                    <?php the_sub_field('quote'); ?>
+                    <cite><?php the_sub_field('nome_quote'); ?></cite>
+                </blockquote>
+        <?php endwhile; else : endif; ?>
     </section>
 
     <section class="portfolio">
