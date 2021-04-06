05270a00ee2d706d790f1dbeed5e3cd061fb0586
Bárbara Alves <barbaraalves343@gmail.com>
2021-04-06 17:19:17 -0300

    Alterando informações de orçamento para funções

--- page-produtos.php
+++ page-produtos.php
@@ -94,20 +94,7 @@ get_header();
                 <textarea id="mensagem" name="mensagem" required></textarea>
                 <button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
             </form>
-            <div class="orcamento-dados grid-8">
-                <h3>Dados</h3>
-                <span>+55 21 93223 3232</span>
-                <span>orcamento@bikcraft.com</span>
-                <h3>Monte a sua Bikcraft</h3>
-                <p>Escolha as especificações:</p>
-                <ul>
-                    <li>- Cores</li>
-                    <li>- Estilo</li>
-                    <li>- Medidas</li>
-                    <li>- Acessórios</li>
-                    <li>- E Outros</li>
-                </ul>
-            </div>
+            <?php include(TEMPLATEPATH . "/inc/produtos-orcamento.php"); ?>
         </div>
     </section>
     
