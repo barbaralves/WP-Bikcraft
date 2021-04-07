059e14aa93ac6e66142ce82e8050b14fb0d2cd06
Bárbara Alves <barbaraalves343@gmail.com>
2021-04-06 21:43:50 -0300

    Alterando ul e li para menu do WP

--- header.php
+++ header.php
@@ -30,12 +30,14 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/img/bikcraft.svg" alt="Bikcraft">
             </a>
             <nav class="grid-12 header_menu">
-                <ul>
-                    <li><a href="/sites/bikcraft/sobre/">Sobre</a></li>
-                    <li><a href="/sites/bikcraft/produtos/">Produtos</a></li>
-                    <li><a href="/sites/bikcraft/portfolio/">Portfólio</a></li>
-                    <li><a href="/sites/bikcraft/contato/">Contato</a></li>
-                </ul>
+            <?php
+                $args = array(
+                    'menu' => 'principal',
+                    'theme_location' => 'menu-principal',
+                    'container' => false
+                );
+                wp_nav_menu( $args );
+            ?>
             </nav>
         </div>
     </header>
